6dddf962d0ed7638af7ef977ef3b600b823cf35b
Victor <993113161@qq.com>
2017-02-11 01:41:53 +0800

    Initial Commit

---
+++ SqList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SqList
+//
+//  Created by Imac on 17/2/11.
+//  Copyright © 2017年 Imac. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
