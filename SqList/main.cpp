5254d01f9f567c708332e0147e24661e7e29ce71
Victor <993113161@qq.com>
2017-03-10 07:48:39 +0800

    Revert "add hello world"
    This reverts commit 18096299f28998d824101030500527062dfec746.

--- SqList/main.cpp
+++ SqList/main.cpp
@@ -7,66 +7,9 @@
 //
 
 #include <iostream>
-#include <stdlib.h>
-using namespace std;
 
-const int maxSize = 100;
-
-typedef struct SLNode
-{
-    int data[maxSize];
-    int length;
-} Sqlist;
-
-//找到元素在顺序表中的位置
-int LocateElem(Sqlist L,int e)
-{
-    int i;    //元素下标
-    for(i=0; i<L.length; i++)
-    {
-        if(e==L.data[i])
-        {
-            return i;
-        }
-    }
-    return i;
-}
-
-//在顺序表中插入一个元素
-int insert(Sqlist &L, int p, int e)
-{
-    int i;
-    if( p<1 || p>L.length+1 || L.length>=maxSize-1 )
-    {
-        return 0; //插入失败返回0
-    }
-    for(i=L.length-1; i>=p-1; i++)
-    {
-        L.data[i+1]=L.data[i];
-    }
-    L.data[p]=e;
-    L.length++;
-    return 1; //插入成功返回1
-}
-
-void visit(Sqlist L)
-{
-    int i;
-    for(i=0; i<=L.length; i++)
-    {
-        printf("第%d个元素是： %d\n",i+1,L.data[i]);
-    }
-}
-
-int main(int argc, const char * argv[])
-{
-    Sqlist *L;
-    L=(Sqlist *)malloc(sizeof(Sqlist));
-    for(int i=0; i<5; i++)
-    {
-        insert(*L, i, i+1);
-    }
-    visit(*L);
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
     return 0;
 }
-
