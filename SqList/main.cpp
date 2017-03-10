46959befc3f565b3fe32f5088555e00b0c3d8de7
Victor <993113161@qq.com>
2017-03-10 09:24:35 +0800

    add namespace

---
+++ SqList/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  SqList
+//
+//  Created by Imac on 17/2/11.
+//  Copyright © 2017年 Imac. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
